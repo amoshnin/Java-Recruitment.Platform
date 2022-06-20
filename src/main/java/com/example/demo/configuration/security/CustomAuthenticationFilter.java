ce0ba59441c59a70b87c2461350c0d8281b77c2b
Artem.Moshnin <artem.moshnin@sqa-consulting.com>
2022-06-20 14:09:48 +0200

    s

--- src/main/java/com/example/demo/configuration/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/demo/configuration/security/CustomAuthenticationFilter.java
@@ -53,7 +53,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .sign(algorithm);
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 999999999L * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 5 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
         Map<String, String> tokens = new HashMap<>();
