cb759c8868cead318b039b767c19ae203dbdd7d5
Artem.Moshnin <artem.moshnin@sqa-consulting.com>
2022-06-23 11:40:51 +0200

    writing unit tests

--- src/test/java/com/example/demo/models/job/JobService_Add_Tests.java
+++ src/test/java/com/example/demo/models/job/JobService_Add_Tests.java
@@ -1,35 +1,22 @@
 package com.example.demo.models.job;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class JobServiceTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobService_Add_Tests {
     @Autowired
     private JobService service;
 
     @MockBean
-    private JobRepository repository;
+    private JobRepository jobRepository;
 
     @Test
     void add() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void getItem() {
-    }
-
-    @Test
-    void getMyListAsRecruiter() {
-    }
-
-    @Test
-    void getAllListAsAdmin() {
     }
 }
---
+++ src/test/java/com/example/demo/models/job/JobService_GetItem_Tests.java
@@ -0,0 +1,23 @@
+package com.example.demo.models.job;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobService_GetItem_Tests {
+    @Autowired
+    private JobService service;
+
+    @MockBean
+    private JobRepository jobRepository;
+
+    @Test
+    void getItem() {
+    }
+}
+
