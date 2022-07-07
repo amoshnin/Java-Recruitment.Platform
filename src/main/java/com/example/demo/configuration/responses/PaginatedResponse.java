f423a1b7fc0b04c207ecc3f7f2cc90ac6636349a
Artem.Moshnin <artem.moshnin@sqa-consulting.com>
2022-07-07 10:54:56 +0200

    s

--- src/main/java/com/example/demo/configuration/responses/PaginatedResponse.java
+++ src/main/java/com/example/demo/configuration/responses/PaginatedResponse.java
@@ -7,11 +7,13 @@ import java.util.List;
 public class PaginatedResponse<T> {
     public int recordCount;
     public int totalNumberOfPages;
+    public int pageNumber;
     public List<T> response;
 
     public PaginatedResponse(Page<T> page) {
-        this.totalNumberOfPages = page.getTotalPages();
+        this.totalNumberOfPages = page.getTotalPages() - 1;
         this.recordCount = page.getNumberOfElements();
         this.response = page.getContent();
+        this.pageNumber = page.getNumber();
     }
 }
