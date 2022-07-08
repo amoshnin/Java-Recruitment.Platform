0870d6481ecd69e8e2285eff7f9ddf15ba6f5be7
Artem.Moshnin <artem.moshnin@sqa-consulting.com>
2022-07-08 10:37:24 +0200

    s

--- src/main/java/com/example/demo/configuration/responses/PaginatedResponse.java
+++ src/main/java/com/example/demo/configuration/responses/PaginatedResponse.java
@@ -5,15 +5,13 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 public class PaginatedResponse<T> {
-    public int recordCount;
+    public int pageSize;
     public int totalNumberOfPages;
-    public int pageNumber;
     public List<T> response;
 
     public PaginatedResponse(Page<T> page) {
         this.totalNumberOfPages = page.getTotalPages() - 1;
-        this.recordCount = page.getNumberOfElements();
+        this.pageSize = page.getNumberOfElements();
         this.response = page.getContent();
-        this.pageNumber = page.getNumber();
     }
 }
